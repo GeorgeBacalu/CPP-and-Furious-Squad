c40fd6183d2859d40c0cb4ea816d34a52c9de8c7
ioan.chitu@student.unitbv.ro <ioan.chitu@student.unitbv.ro>
2023-10-23 11:49:35 +0300

    Setter and Getter for ScoreBoard

--- CPP-and-Furious-Squad/ScoreBoard.cpp
+++ CPP-and-Furious-Squad/ScoreBoard.cpp
@@ -1,6 +1,6 @@
 #include "ScoreBoard.h"
 
-ScoreBoard::ScoreBoard() : score{ 0, 0 }
+ScoreBoard::ScoreBoard() : m_score{ 0, 0 }
 {
 }
 
@@ -8,7 +8,7 @@ ScoreBoard::~ScoreBoard()
 {
 }
 
-ScoreBoard::ScoreBoard(const ScoreBoard& other) : score{ other.score }
+ScoreBoard::ScoreBoard(const ScoreBoard& other) : m_score{ other.m_score }
 {
 }
 
@@ -16,7 +16,23 @@ ScoreBoard& ScoreBoard::operator=(const ScoreBoard& other)
 {
 	if (this != &other)
 	{
-		score = other.score;
+		m_score = other.m_score;
 	}
 	return *this;
+}
+
+std::pair<uint16_t, uint16_t> ScoreBoard::GetScore() const
+{
+	return m_score;
+}
+
+void ScoreBoard::SetScore(std::pair<uint16_t, uint16_t> score)
+{
+	m_score = score;
+}
+
+void ScoreBoard::SetScore(uint16_t scoreRed, uint16_t scoreBlack)
+{
+	m_score.first = scoreRed;
+	m_score.second = scoreBlack;
 }
--- CPP-and-Furious-Squad/ScoreBoard.h
+++ CPP-and-Furious-Squad/ScoreBoard.h
@@ -4,12 +4,14 @@
 
 class ScoreBoard
 {
-	std::pair<uint16_t, uint16_t> score;
+	std::pair<uint16_t, uint16_t> m_score;
 public:
 	ScoreBoard();
 	~ScoreBoard();
 	ScoreBoard(const ScoreBoard& other);
 	ScoreBoard& operator=(const ScoreBoard& other);
-
+	std::pair<uint16_t, uint16_t> GetScore() const;
+	void SetScore(std::pair<uint16_t, uint16_t> score);
+	void SetScore(uint16_t scoreRed, uint16_t scoreBlack);
 };
 
