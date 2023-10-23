aef7b421cfd8f0fa6b5d297259c12869756b34fd
ioan.chitu@student.unitbv.ro <ioan.chitu@student.unitbv.ro>
2023-10-23 11:40:14 +0300

    ScoreBoard header

---
+++ CPP-and-Furious-Squad/ScoreBoard.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <iostream>
+
+class ScoreBoard
+{
+	std::pair<uint16_t, uint16_t> score;
+public:
+	ScoreBoard();
+	~ScoreBoard();
+	ScoreBoard(const ScoreBoard& other);
+	ScoreBoard& operator=(const ScoreBoard& other);
+};
+
