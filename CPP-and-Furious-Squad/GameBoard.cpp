4c9ed38893f29af2a051c067e8da564bdfc844c1
Csere Cosmin <cserecosmin@yahoo.com>
2023-10-23 23:36:57 +0300

    Update Rule of 3 + Getr/Setr

--- CPP-and-Furious-Squad/GameBoard.cpp
+++ CPP-and-Furious-Squad/GameBoard.cpp
@@ -2,18 +2,30 @@
 
 GameBoard::GameBoard()
 {
-	s_size = uint16_t();
-	s_matrix = std::vector<std::vector<std::shared_ptr<Pillar>>>();
+	s_size = 24;
+	for (uint16_t i = 0; i < s_size; ++i)
+	{
+		std::vector<Pillar*> row;
+		for (uint16_t j = 0; j < s_size; ++j)
+		{
+			row.push_back(nullptr);
+		}
+		s_matrix.push_back(row);
+	}
 }
 
-GameBoard::GameBoard(const uint16_t& size, const std::vector<std::vector<std::shared_ptr<Pillar>>>& matrix)
+GameBoard::GameBoard(const uint16_t& size, const std::vector<std::vector<Pillar*>>& matrix)
 {
 	s_size = size;
-	for (int i = 0; i < s_size; i++)
+	s_matrix= std::vector<std::vector<Pillar*>>();
+	for (uint16_t i = 0; i < s_size; ++i)
 	{
-		std::vector<std::shared_ptr<Pillar>> row;
-		for (uint16_t j = 0; j < s_size; ++j) 
-			row.push_back(std::make_shared<Pillar>(matrix[i][j]));
+		std::vector<Pillar*> row;
+		for (uint16_t j = 0; j < s_size; ++j)
+		{
+			Pillar *aux= new Pillar(*matrix[i][j]);
+			row.push_back(aux);
+		}
 		s_matrix.push_back(row);
 	}
 }
@@ -26,8 +38,52 @@ GameBoard::GameBoard(const GameBoard& gb)
 
 GameBoard& GameBoard::operator=(const GameBoard& gb)
 {
-	s_size = gb.s_size;
-	s_matrix = gb.s_matrix;
+
+	if (this != &gb)
+	{
+		s_size = gb.s_size; 
+		s_matrix = gb.s_matrix;
+	}
+	return *this;
+}
+
+uint8_t GameBoard::getSize()
+{
+	return s_size;
+}
+
+std::vector<std::vector<Pillar*>> GameBoard::getMatrix()
+{
+	return s_matrix;
+}
+
+void GameBoard::setSize(uint16_t size)
+{
+	s_size = size;
+}
+
+void GameBoard::setMatrix(std::vector<std::vector<Pillar*>> matrix )
+{
+	s_matrix = std::vector<std::vector<Pillar*>>();
+	for (uint16_t i = 0; i < s_size; ++i)
+	{
+		std::vector<Pillar*> row;
+		for (uint16_t j = 0; j < s_size; ++j)
+		{
+			Pillar* aux = new Pillar(*matrix[i][j]);
+			row.push_back(aux);
+		}
+		s_matrix.push_back(row);
+	}
 }
 
 
+std::ostream& operator<<(std::ostream& out, const GameBoard& gb)
+{
+	for (uint16_t i = 0; i < gb.s_size; i++)
+	{
+		for (uint16_t j = 0; j < gb.s_size; j++)
+			out << gb.s_matrix[i][j];
+	}
+	return out;
+}
--- CPP-and-Furious-Squad/GameBoard.h
+++ CPP-and-Furious-Squad/GameBoard.h
@@ -5,18 +5,20 @@
 class GameBoard
 {	
 	static uint16_t s_size;
-	static std::vector<std::vector<std::shared_ptr<Pillar>>> s_matrix;
+	static std::vector<std::vector<Pillar*>> s_matrix;
 public:
 	//Constructor/Deconstructor/Rule of 3
 	GameBoard();
-	GameBoard(const uint16_t &size, const std::vector<std::vector<std::shared_ptr<Pillar>>>&matrix);
+	GameBoard(const uint16_t &size, const std::vector<std::vector<Pillar*>>&matrix);
 	GameBoard(const GameBoard &gb);
+	GameBoard& operator=(const GameBoard& gb);
+	friend std::ostream& operator<<(std::ostream& out, const GameBoard& gb);
 	~GameBoard();
 	//getR/setR
 	uint8_t getSize();
-	std::vector<std::vector<std::shared_ptr<Pillar>>> getMatrix();
+	std::vector<std::vector<Pillar*>> getMatrix();
 	void setSize(uint16_t size);
-	void setMatrix(std::vector<std::vector<std::shared_ptr<Pillar>>> matrix);
+	void setMatrix(std::vector<std::vector<Pillar*>>);
 	//logic methods
 	void PlacePillar(uint16_t row , uint16_t column);
 	void RemoveePillar(uint16_t row, uint16_t column);
