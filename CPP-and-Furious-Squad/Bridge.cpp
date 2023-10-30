8c7b769b2dd38f0f80fa381f96dcd364a4e011c2
Catalin Buta <catalinbuta12@yahoo.com>
2023-10-30 20:32:34 +0200

    Add getters for Bridge and constructed validation methods

--- CPP-and-Furious-Squad/Bridge.cpp
+++ CPP-and-Furious-Squad/Bridge.cpp
@@ -1,10 +1,16 @@
 #include "Bridge.h"
 
-Bridge::Bridge() : m_startPillar{ Pillar() }, m_endPillar{ Pillar() }
+Color Bridge::GetRandomColor()
+{
+	return rand() % 2 == 1 ? Color::RED : Color::BLACK;
+}
+
+
+Bridge::Bridge() : m_startPillar{ Pillar() }, m_endPillar{ Pillar()}, m_color{GetRandomColor()}
 {
 }
 
-Bridge::Bridge(const Pillar& startPillar, const Pillar& endPillar) : m_startPillar{ startPillar }, m_endPillar(endPillar)
+Bridge::Bridge(const Pillar& startPillar, const Pillar& endPillar, Color color) : m_startPillar{ startPillar }, m_endPillar(endPillar), m_color{color}
 {
 }
 
@@ -14,6 +20,21 @@ Bridge::Bridge(const Bridge& other) : m_startPillar{ other.m_startPillar }, m_en
 
 Bridge::~Bridge()
 {
+}
+
+const Pillar& Bridge::GetStartPillar() const
+{
+	return m_startPillar;
+}
+
+const Pillar& Bridge::GetEndPillar() const
+{
+	return m_endPillar;
+}
+
+Color Bridge::GetColor() const
+{
+	return m_color;
 }
 
 Bridge& Bridge::operator=(const Bridge& other)
@@ -27,6 +48,21 @@ Bridge& Bridge::operator=(const Bridge& other)
 	return*this;
 }
 
+std::istream& operator>>(std::istream& in, Bridge& bridge)
+{
+	if (!(in >> bridge.m_startPillar >> bridge.m_endPillar))
+	{
+		throw std::invalid_argument("Error while reading bridge input.");
+	}
+
+	if (!bridge.IsValid(bridge))
+	{
+		throw std::invalid_argument("Can't build bridge between this pillars.");
+	}
+	
+	return in;
+}
+
 std::ostream& operator<<(std::ostream& out, const Bridge& bridge)
 {
 	out << "Start pillar : " << bridge.m_startPillar << std::endl;
@@ -35,7 +71,27 @@ std::ostream& operator<<(std::ostream& out, const Bridge& bridge)
 	return out;
 }
 
-bool Bridge::isValidBetween(const Pillar& startPillar, const Pillar& endPillar)
+bool Bridge::IsValid(const Bridge& bridge)
+{
+	return CheckSameColor(bridge) && CheckDistinctPositions(bridge) && CheckBridgeValid(bridge) && CheckNoIntersections(bridge);
+}
+
+bool Bridge::CheckSameColor(const Bridge& bridge)
+{
+	throw std::logic_error("Method not yet implemented!");
+}
+
+bool Bridge::CheckDistinctPositions(const Bridge& bridge)
+{
+	throw std::logic_error("Method not yet implemented!");
+}
+
+bool Bridge::CheckBridgeValid(const Bridge& bridge)
+{
+	throw std::logic_error("Method not yet implemented!");
+}
+
+bool Bridge::CheckNoIntersections(const Bridge& bridge)
 {
 	throw std::logic_error("Method not yet implemented!");
 }
--- CPP-and-Furious-Squad/Bridge.h
+++ CPP-and-Furious-Squad/Bridge.h
@@ -4,17 +4,30 @@ class Bridge
 {
 	Pillar m_startPillar;
 	Pillar m_endPillar;
+	Color m_color : 1;
+
+	static Color GetRandomColor();
+
+	bool CheckSameColor(const Bridge& bridge);
+	bool CheckDistinctPositions(const Bridge& bridge);
+	bool CheckBridgeValid(const Bridge& bridge);
+	bool CheckNoIntersections(const Bridge& bridge);
 
 public :
 
 	Bridge();
-	Bridge(const Pillar& startPillar, const Pillar& endPillar);
+	Bridge(const Pillar& startPillar, const Pillar& endPillar, Color color);
 	Bridge(const Bridge& other);
 	~Bridge();
 
+	const Pillar& GetStartPillar() const;
+	const Pillar& GetEndPillar() const;
+	Color GetColor() const;
+
 	Bridge& operator=(const Bridge& other);
+	friend std::istream& operator>>(std::istream& in, Bridge& bridge);
 	friend std::ostream& operator<<(std::ostream& out, const Bridge& bridge);
 
-	bool isValidBetween(const Pillar& startPillar, const Pillar& endPillar);
+	bool IsValid(const Bridge& bridge);
 };
 
