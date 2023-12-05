b6edd843ed45d9013930a30e216069b0a0ccf1f5
Csere Cosmin <cserecosmin@yahoo.com>
2023-12-05 17:45:44 +0200

    Implemented the body of CircleWidget

---
+++ Window/CircleWidget.cpp
@@ -0,0 +1,18 @@
+#include "CircleWidget.h"
+
+void CircleWidget::setColor(const QColor& newColor)
+{
+	color = newColor;
+	update();
+}
+
+void CircleWidget::paintEvent(QPaintEvent* event)
+{
+    Q_UNUSED(event);
+
+    QPainter painter(this);
+    painter.setRenderHint(QPainter::Antialiasing, true);
+    painter.setBrush(color);
+    QRect ellipseRect(rect().center().x() - 10, rect().center().y() - 10, 20, 20);
+    painter.drawEllipse(ellipseRect);
+}
